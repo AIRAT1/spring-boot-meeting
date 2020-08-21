4bc6cc6d046db6a25c57b04cc17e4a10d319fc86
Ayrat <ayrat1@mail.ru>
2020-08-21 21:27:19 +0300

    add integration test

--- src/main/java/de/springboot/model/StackoverflowWebsite.java
+++ src/main/java/de/springboot/model/StackoverflowWebsite.java
@@ -2,17 +2,21 @@ package de.springboot.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @AllArgsConstructor
+@NoArgsConstructor
 @Getter
+@Setter
 @Document
 public class StackoverflowWebsite {
     @Id
-    private final String id;
-    private final String website;
-    private final String iconImageUrl;
-    private final String title;
-    private final String description;
+    private String id;
+    private String website;
+    private String iconImageUrl;
+    private String title;
+    private String description;
 }
---
+++ src/test/java/de/springboot/web/StackOverflowControllerIT.java
@@ -0,0 +1,56 @@
+package de.springboot.web;
+
+import com.google.common.collect.ImmutableList;
+import de.springboot.model.StackoverflowWebsite;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+
+import static java.util.stream.Collectors.collectingAndThen;
+import static java.util.stream.Collectors.toList;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsInAnyOrder;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest
+public class StackOverflowControllerIT {
+    RestTemplate restTemplate = new RestTemplate();
+
+    @Autowired
+    private MongoTemplate mongoTemplate;
+
+    @Before
+    public void before() {
+        mongoTemplate.dropCollection(StackoverflowWebsite.class);
+        mongoTemplate.save(new StackoverflowWebsite("website1", null, null, null, null));
+        mongoTemplate.save(new StackoverflowWebsite("website2", null, null, null, null));
+    }
+
+    @Test
+    public void testGetListOfProviders() throws Exception {
+        // test
+        ResponseEntity<List<StackoverflowWebsite>> responseEntity = restTemplate.exchange("http://localhost:8080/api/stackoverflow",
+                HttpMethod.GET,
+                null,
+                new ParameterizedTypeReference<List<StackoverflowWebsite>>() {
+                });
+        List<StackoverflowWebsite> actualList = responseEntity.getBody();
+        // validate
+        assertThat(actualList.size(), is(2));
+        List<String> actualIds = actualList.stream().map(stackoverflowWebsite ->
+                stackoverflowWebsite.getId()).collect(collectingAndThen(toList(), ImmutableList::copyOf));
+        assertThat(actualIds, containsInAnyOrder("website1", "website2"));
+
+    }
+}
